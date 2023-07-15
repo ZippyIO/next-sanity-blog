5b3126167e26452558193963983ffb5e03896196
Nick <delta2888@gmail.com>
2023-07-15 14:56:45 +1000

    feat: add article sanity schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,6 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from 'sanity';
+import { articleSchema } from './schemas/article';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [articleSchema],
+};
---
+++ sanity/schemas/article.ts
@@ -0,0 +1,19 @@
+import { defineField, defineType } from 'sanity';
+
+export const articleSchema = defineType({
+  name: 'article',
+  title: 'Article',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'content',
+      title: 'Content',
+      type: 'string',
+    }),
+  ],
+});
